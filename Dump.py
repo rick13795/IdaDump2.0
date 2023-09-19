1b8007569de94524e1bcad19ceada1483d44e9a9
rick13795 <102271237+rick13795@users.noreply.github.com>
2023-09-19 19:46:08 +0800

    Create Dump.py
    ida版本7.6以上

---
+++ Dump.py
@@ -0,0 +1,81 @@
+import idaapi
+import idautils
+import idc
+import os
+improt magic
+
+class ELFExporterPlugin(idaapi.plugin_t):
+    flags = idaapi.PLUGIN_PROC | idaapi.PLUGIN_MOD
+    comment = "Export ELF segments"
+    help = "Export ELF segments to a file"
+    wanted_name = "ELF Exporter"
+    wanted_hotkey = ""
+
+    def init(self):
+        return idaapi.PLUGIN_KEEP
+
+    def run(self, arg):
+        self.export_elf_segments()
+
+    def term(self):
+        pass
+
+   def export_elf_segments(self, export_address=None):
+    base_addr = idaapi.get_imagebase()
+    elf_file_path = idc.AskFile(1, "*.elf", "Choose an ELF file to export segments")
+
+    if not elf_file_path:
+        return
+
+    with open(elf_file_path, "wb") as dumpfile:
+        for segment_ea in idautils.Segments():
+            seg = idaapi.getseg(segment_ea)
+            if seg:
+                seg_start = seg.start_ea
+                seg_end = seg.end_ea
+                seg_size = seg_end - seg_start
+                seg_offset = seg_start - base_addr
+
+                # Ensure the segment is within the ELF image
+                if seg_offset >= 0:
+                    print(f"Exporting segment {seg} to file...")
+                    self.dump_segment(dumpfile, seg_start, seg_size, seg_offset)
+
+        print(f"Export completed. Segments saved to {elf_file_path}")
+
+    if export_address:
+        export_file_path = idc.AskFile(1, "*.bin", "Choose a destination file to export data")
+
+        if not export_file_path:
+            return
+
+        segment = idaapi.getseg(export_address)
+        if segment:
+            seg_start = segment.start_ea
+            seg_end = segment.end_ea
+            seg_size = seg_end - seg_start
+            seg_offset = seg_start - base_addr
+
+            # Ensure the selected address is within the ELF image
+            if seg_offset >= 0:
+                with open(export_file_path, "wb") as exportfile:
+                    self.dump_segment(exportfile, seg_start, seg_size, seg_offset)
+                print(f"Exported data at address {hex(export_address)} to {export_file_path}")
+
+  def run(self, arg):
+    if arg:
+        export_address = idc.AskLong(0, "Enter the address to export data from (hex):")
+        if export_address is None:
+            return
+        self.export_elf_segments(export_address)
+    else:
+        self.export_elf_segments()
+
+
+  def dump_segment(self, dumpfile, start, size, offset):
+        dumpfile.seek(offset)
+        for i in range(size):
+            dumpfile.write(bytes([idc.get_wide_byte(start + i)]))
+
+def PLUGIN_ENTRY():
+    return ELFExporterPlugin()
